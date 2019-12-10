78a90398a2b7fc3bb5e340bf601d4d9a9f0a2806
Kiaradamiancoloma <57924219+kiaradamiancoloma@users.noreply.github.com>
2019-12-10 15:59:43 -0500

    lote.bat

---
+++ lote.bat
@@ -0,0 +1,264 @@
+python Damian/simple01.py 6 7
+python Damian/simple01.py 8 9
+python Damian/simple01.py 10 7
+
+python Damian/simple02.py 2 6
+python Damian/simple02.py 5 7
+python Damian/simple02.py 8 9
+
+python Damian/simple03.py 15 45
+python Damian/simple03.py 25 30
+python Damian/simple03.py 6  17
+
+python Damian/simple04.py 1000 60
+python Damian/simple04.py 2000 50
+python Damian/simple04.py 3000 80
+
+python Damian/simple05.py  15 16 5
+python Damian/simple05.py  18 17 6
+python Damian/simple05.py  19 11 8
+
+python Damian/simple06.py 123.4 134.6 178.9
+python Damian/simple06.py 139.2 104.5 168.9
+python Damian/simple06.py 125.1 114.6 178.9
+
+python Damian/simple07.py 100 200
+python Damian/simple07.py 400 250
+python Damian/simple07.py 200 400
+
+python Damian/simple08.py 89.6 7.8
+python Damian/simple08.py 97.6 5.3
+python Damian/simple08.py 23.6 4.4
+
+python Damian/simple09.py 7 6 9
+python Damian/simple09.py 11 5 19
+python Damian/simple09.py 15 4 9
+
+python Damian/simple10.py 9 7
+python Damian/simple10.py 6 5
+python Damian/simple10.py 19 5
+
+python Damian/doble01.py 6 7
+python Damian/doble01.py 8 9
+python Damian/doble01.py 10 7
+
+python Damian/doble02.py 2 6
+python Damian/doble02.py 5 7
+python Damian/doble02.py 8 9
+
+python Damian/doble03.py 15 45
+python Damian/doble03.py 25 30
+python Damian/doble03.py 6  17
+
+python Damian/doble04.py 1000 60
+python Damian/doble04.py 2000 50
+python Damian/doble04.py 3000 80
+
+python Damian/doble05.py  15 16 5
+python Damian/doble05.py  18 17 6
+python Damian/doble05.py  19 11 8
+
+python Damian/doble06.py 123.4 134.6 178.9
+python Damian/doble06.py 139.2 104.5 168.9
+python Damian/doble06.py 125.1 114.6 178.9
+
+python Damian/doble07.py 100 200
+python Damian/doble07.py 400 250
+python Damian/doble07.py 200 400
+
+python Damian/doble08.py 89.6 7.8
+python Damian/doble08.py 97.6 5.3
+python Damian/doble08.py 23.6 4.4
+
+python Damian/doble09.py 7 6 9
+python Damian/doble09.py 11 5 19
+python Damian/doble09.py 15 4 9
+
+python Damian/doble10.py 9 7
+python Damian/doble10.py 6 5
+python Damian/doble10.py 19 5
+
+python Damian/multiple01.py 6 7
+python Damian/multiple01.py 8 9
+python Damian/multiple01.py 10 7
+
+python Damian/multiple02.py 2 6
+python Damian/multiple02.py 5 7
+python Damian/multiple02.py 8 9
+
+python Damian/multiple03.py 15 45
+python Damian/multiple03.py 25 30
+python Damian/multiple03.py 6  17
+
+python Damian/multiple04.py 1000 60
+python Damian/multiple04.py 2000 50
+python Damian/multiple04.py 3000 80
+
+python Damian/multiple05.py  15 16 5
+python Damian/multiple05.py  18 17 6
+python Damian/multiple05.py  19 11 8
+
+python Damian/multiple06.py 123.4 134.6 178.9
+python Damian/multiple06.py 139.2 104.5 168.9
+python Damian/multiple06.py 125.1 114.6 178.9
+
+python Damian/multiple07.py 100 200
+python Damian/multiple07.py 400 250
+python Damian/multiple07.py 200 400
+
+python Damian/multiple08.py 89.6 7.8
+python Damian/multiple08.py 97.6 5.3
+python Damian/multiple08.py 23.6 4.4
+
+python Damian/multiple09.py 7 6 9
+python Damian/multiple09.py 60 5 24
+python Damian/multiple09.py 10 20 15
+
+python Damian/multiple10.py 1.4 0.7
+python Damian/multiple10.py 1.5 3.5
+python Damian/multiple10.py 2.0 1.7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+python Mendoza/Simple01.py 4
+python Mendoza/Simple01.py 12
+python Mendoza/Simple01.py 144
+
+python Mendoza/Simple02.py 9
+python Mendoza/Simple02.py 13
+python Mendoza/Simple02.py 17
+
+python Mendoza/Simple03.py 15 0
+python Mendoza/Simple03.py 0 9
+python Mendoza/Simple03.py 700 0
+
+python Mendoza/Simple04.py 90
+python Mendoza/Simple04.py 14
+python Mendoza/Simple04.py 2
+
+python Mendoza/Simple05.py 18 1000
+python Mendoza/Simple05.py 25 4500
+python Mendoza/Simple05.py 35 2000
+
+python Mendoza/Simple06.py 20
+python Mendoza/Simple06.py 45
+python Mendoza/Simple06.py 10
+
+python Mendoza/Simple07.py "a"
+python Mendoza/Simple07.py "i"
+python Mendoza/Simple07.py "u"
+
+python Mendoza/Simple08.py 14 3
+python Mendoza/Simple08.py 190 34
+python Mendoza/Simple08.py 67 45
+
+python Mendoza/Simple09.py 3 20
+python Mendoza/Simple09.py 10 15
+python Mendoza/Simple09.py 20 10
+
+python Mendoza/Simple10.py 2.5 1 5
+python Mendoza/Simple10.py 6.5 1 6
+python Mendoza/Simple10.py 13.5 1.5 7
+
+python Mendoza/Doble01.py -15
+python Mendoza/Doble01.py 20
+python Mendoza/Doble01.py 0
+
+python Mendoza/Doble02.py 17
+python Mendoza/Doble02.py 20
+python Mendoza/Doble02.py 15
+
+python Mendoza/Doble03.py 100 0
+python Mendoza/Doble03.py 0 28
+python Mendoza/Doble03.py 60 3
+
+python Mendoza/Doble04.py 34
+python Mendoza/Doble04.py 71
+python Mendoza/Doble04.py 367
+
+python Mendoza/Doble05.py 16 1000
+python Mendoza/Doble05.py 20 700
+python Mendoza/Doble05.py 18 1500
+
+python Mendoza/Doble06.py 51
+python Mendoza/Doble06.py 89
+python Mendoza/Doble06.py 4
+
+python Mendoza/Doble07.py "a"
+python Mendoza/Doble07.py "c"
+python Mendoza/Doble07.py "g"
+
+python Mendoza/Doble08.py 106 10
+python Mendoza/Doble08.py 90 567
+python Mendoza/Doble08.py 0 76
+
+python Mendoza/Doble09.py 2 20
+python Mendoza/Doble09.py 1 35
+python Mendoza/Doble09.py 3 15
+
+python Mendoza/Doble10.py "D"
+python Mendoza/Doble10.py "A"
+python Mendoza/Doble10.py "B"
+
+python Mendoza/Multiple01.py 3500
+python Mendoza/Multiple01.py 900
+python Mendoza/Multiple01.py 610000
+
+python Mendoza/Multiple02.py 0
+python Mendoza/Multiple02.py 35
+python Mendoza/Multiple02.py 16
+
+python Mendoza/Multiple03.py 56 1.63
+python Mendoza/Multiple03.py 50 1.47
+python Mendoza/Multiple03.py 63 1.70
+
+python Mendoza/Multiple04.py 89
+python Mendoza/Multiple04.py -6
+python Mendoza/Multiple04.py 0
+
+python Mendoza/Multiple05.py 17 900
+python Mendoza/Multiple05.py 35 8500
+python Mendoza/Multiple05.py 26 3500
+
+python Mendoza/Multiple06.py 3000 0.1 5
+python Mendoza/Multiple06.py 6000 0.4 10
+python Mendoza/Multiple06.py 30000 0.2 15
+
+python Mendoza/Multiple07.py "o"
+python Mendoza/Multiple07.py "7"
+python Mendoza/Multiple07.py "b"
+
+python Mendoza/Multiple08.py 34 12
+python Mendoza/Multiple08.py 90 90
+python Mendoza/Multiple08.py 56 105
+
+python Mendoza/Multiple09.py 120 15
+python Mendoza/Multiple09.py 5 20
+python Mendoza/Multiple09.py 2 30
+
+python Mendoza/Multiple10.py "LIBRA"
+python Mendoza/Multiple10.py "VIRGO"
+python Mendoza/Multiple10.py "SAGITARIO"
+
+
