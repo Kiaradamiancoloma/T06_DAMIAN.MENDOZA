cdbff3ce644cac64479e9cecf4e85ad213664f7e
mendoza gonzales <imendozag@unprg.edu.pe>
2019-12-10 15:38:30 -0500

    condicional simple04

---
+++ Damian/simple04.py
@@ -0,0 +1,16 @@
+#Calcular la ganancia que tendra el vendedor
+#Declaracion
+precio_venta=0
+precio_compra=0
+ganancia=0
+import os
+
+#Input
+precio_compra=int(os.sys.argv[1])
+precio_venta=int(os.sys.argv[2])
+
+#Processing
+ganancia=precio_compra-precio_venta
+
+if (ganancia>100):
+    print("la ganancia es:",precio_compra-precio_venta)
